c624c838f2d35c4fce529a9a2a87874d2de6c265
John Stanton-Geddes <john.stantongeddes.research@gmail.com>
2016-03-22 22:31:03 -0400

    added balanced accuracy for scoring models

--- model_evaluation/score_models.R
+++ model_evaluation/score_models.R
@@ -11,7 +11,8 @@ results_df <- data.frame(
   Model = as.character(),
   Accuracy = as.numeric(),
   Sensitivity = as.numeric(),
-  Specificity = as.numeric()
+  Specificity = as.numeric(),
+  Balanced_Accuracy = as.numeric()
 )
 
 # for each file
@@ -21,9 +22,13 @@ for(f in files) {
   # model name
   model_name <- unlist(strsplit(f, split = ".csv"))
   
-  # load each file
-  tempfile <- read_csv(f)
+  # try to load file, break if not able to
+  tempfile <- try(read_csv(f))
   pred_vals <- as.vector(unlist(tempfile[ ,2]))
+  
+  # check that the same length as test response. drop if not
+  if(length(pred_vals) != length(test_response)) break
+  
   # compare to true values
   tt <- table(pred_vals, test_response)
   
@@ -34,16 +39,16 @@ for(f in files) {
   acc <- cm$overall["Accuracy"]
   sen <- cm$byClass["Sensitivity"]
   spe <- cm$byClass["Specificity"]
+  bacc <- cm$byClass["Balanced Accuracy"]
   
   # save to dataframe
-  out <- data.frame(Model = model_name, Accuracy = acc, Sensitivity = sen, Specificity = spe)
+  out <- data.frame(Model = model_name, Accuracy = acc, Sensitivity = sen, Specificity = spe, Balanced_Accuracy = bacc)
   results_df <- rbind(results_df, out)
 }
 
 
-
 # order results and save as csv
-results_df <- results_df[order(results_df$Accuracy), ]
+results_df <- results_df[order(desc(results_df$Balanced_Accuracy)), ]
 write_csv(results_df, path = paste0("../statsathon_results_", Sys.Date(), ".csv"))
 
 # format and save as md table
@@ -51,14 +56,18 @@ rownames(results_df) <- as.character(results_df[ , "Model"])
 results_df <- results_df[ , -1]
 
 # custom formatting for github markdown
-cat(paste0("|", "    &nbsp;    ", "|  ", colnames(results_df)[1], "  |  ", colnames(results_df)[2], "  |  ", colnames(results_df)[3], "  |  \n",
-    "| ------------ | ---------- | ------------- | ------------- | \n"), file = "README.md", append = FALSE)
+cat(paste0("|", "    &nbsp;    ", "|  ", colnames(results_df)[1], "  |  ", colnames(results_df)[2], "  |  ", colnames(results_df)[3], "  |  ", colnames(results_df)[4], "  |  \n",
+           "| ------------ | ---------- | ------------- | ------------- | ------------------- | \n"), file = "README.md", append = FALSE)
+
+
+message("Saving results...")
 
 for(row in 1:nrow(results_df)) {
   # append to table
   cat(paste0("| ", "**", rownames(results_df)[row], "** |   ",
              round(results_df[row, "Accuracy"], 4), "   |    ",
              round(results_df[row, "Sensitivity"], 4), "     |      ",
-             round(results_df[row, "Specificity"], 4), "   | \n"),
+             round(results_df[row, "Sensitivity"], 4), "     |      ",
+             round(results_df[row, "Balanced_Accuracy"], 5), "   | \n"),
       file = "README.md", append = TRUE)
 }
